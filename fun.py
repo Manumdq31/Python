79fbd0e98ac19279a8515749fc77d276f55f455b
Manumdq31 <manuolivetto@gmail.com>
2023-02-03 13:45:38 +0100

    Validando

--- fun.py
+++ fun.py
@@ -7,3 +7,20 @@ def valida_contenido (valor):
     else:
         return False
 
+def valida_set(dato1,dato2,numero_juego):
+
+    print("Numero de juego :",numero_juego)
+
+    if dato1 <= 5 and dato2 <= 5 :
+        print("Juego incompleto")        
+    if dato1 == 6 and dato2 <= 4 :
+        print("Ganado por la pareja 1")
+    if dato1 == 7 and dato2 <= 6 :
+        print("Ganado por la pareja 1 en Tie-Break")
+    if dato2 == 6 and dato1 <= 4 :
+        print("Ganado por la pareja 2")
+    if dato2 == 7 and dato1 <= 6 :
+        print("Ganado por la pareja 2 en Tie-Break")
+    
+
+
